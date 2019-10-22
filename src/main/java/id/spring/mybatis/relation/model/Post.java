6b5eb0b7035da94c92637617c9f520d182e8e871
Teten Nugraha <tennugraha777@gmail.com>
2019-10-22 15:45:23 +0700

    add entity

---
+++ src/main/java/id/spring/mybatis/relation/model/Post.java
@@ -0,0 +1,23 @@
+package id.spring.mybatis.relation.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Post {
+	
+	private Integer id;
+	private String title;
+	private String content;
+	private String createdOn;
+	private User user;
+	
+	
+	
+}
---
+++ src/main/java/id/spring/mybatis/relation/model/User.java
@@ -0,0 +1,21 @@
+package id.spring.mybatis.relation.model;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class User {
+	
+	private Integer id;
+	private String emailid;
+	private String name;
+	private List<Post> posts;
+	
+}
