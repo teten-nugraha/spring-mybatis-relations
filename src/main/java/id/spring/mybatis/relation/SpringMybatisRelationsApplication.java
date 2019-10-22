feba047523d2fb11c9970ead08bfa71082413ad2
Teten Nugraha <tennugraha777@gmail.com>
2019-10-22 15:38:35 +0700

    first

---
+++ src/main/java/id/spring/mybatis/relation/SpringMybatisRelationsApplication.java
@@ -0,0 +1,13 @@
+package id.spring.mybatis.relation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMybatisRelationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMybatisRelationsApplication.class, args);
+	}
+
+}
